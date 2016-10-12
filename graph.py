Fixing broadcasting

graph.py
@@ -163,15 +163,17 @@ class Graph(object):
             gradients = y.op.gradient(grad_y)
             assert len(y.op.inputs) == len(gradients)
 
-            for inp, grad in zip(y.op.inputs, gradients):
-                if inp in grads:
-                    grads[inp] += grad
-                else:
-                    grads[inp] = grad
+            for input_, grad in zip(y.op.inputs, gradients):
+                assert np.array_equal(input_.shape, grad.shape), 'gradient shape much match input: {} != {}'.format(input_, grad)
 
-                if not inp.op:
+                if input_ in grads:
+                    grads[input_] += grad
+                else:
+                    grads[input_] = grad
+
+                if not input_.op:
                     continue
 
-                queue.append((inp, grad))
+                queue.append((input_, grad))
 
         return [grads[x] for x in xs]

ops.py
@@ -4,27 +4,7 @@ from __future__ import division
 
 import numpy as np
 
-from utils import reduced_shape, safe_div
-
-def broadcast_shape(shape_a, shape_b):
-    rank_a = len(shape_a)
-    rank_b = len(shape_b)
-    if rank_a > rank_b:
-        shape = shape_a
-    elif rank_a < rank_b:
-        shape = shape_b
-    else:
-        shape = []
-        for dim_a, dim_b in zip(shape_a, shape_b):
-            if dim_a == dim_b:
-                shape.append(dim_a)
-            elif dim_a == 1:
-                shape.append(dim_b)
-            elif dim_b == 1:
-                shape.append(dim_a)
-            else:
-                raise ValueError('Shapes incompatible for broadcasting')
-    return shape
+from utils import reduced_shape, safe_div, broadcast_shape
 
 class BaseOp(object):
     """
@@ -67,7 +47,14 @@ class AddOp(BaseOp):
         return context[a] + context[b]
 
     def gradient(self, grad):
-        return [grad, grad]
+        # x = self.inputs[0]
+        # y = self.inputs[1]
+        # sx = x.shape
+        # sy = y.shape
+        # rx, ry = broadcast_gradient_args(sx, sy)
+        # return [self.graph.reshape(self.graph.sum(grad, axis=rx), sx),
+        #         self.graph.reshape(self.graph.sum(grad, axis=ry), sy)]
+        return [grad * self.graph.ones_like(self.inputs[0]), grad * self.graph.ones_like(self.inputs[1])]
 
 class SubOp(BaseOp):
 
@@ -81,7 +68,7 @@ class SubOp(BaseOp):
         return context[a] - context[b]
 
     def gradient(self, grad):
-        return [grad, -grad]
+        return [grad * self.graph.ones_like(self.inputs[0]), -grad * self.graph.ones_like(self.inputs[1])]
 
 class MulOp(BaseOp):
 
@@ -368,11 +355,8 @@ class TileOp(BaseOp):
         self.reps = reps
 
     def compute(self, context):
-        input_shape = self.inputs[0].shape
-        output_shape = self.output.shape
         x = context[self.inputs[0]]
-        result = np.tile(x, self.reps)
-        return result
+        return np.tile(x, self.reps)
 
 class SumOp(BaseOp):
 
@@ -393,7 +377,8 @@ class SumOp(BaseOp):
         output_shape_kept_dims = reduced_shape(input_shape, op.axis, keep_dims=True)
         tile_scaling = safe_div(input_shape, output_shape_kept_dims)
         grad = op.graph.reshape(grad, output_shape_kept_dims)
-        return [op.graph.tile(grad, tile_scaling)]
+        tile = op.graph.tile(grad, tile_scaling)
+        return [tile]
 
     def gradient(self, grad):
         return SumOp.gradient_static(self, grad)
@@ -412,7 +397,7 @@ class MeanOp(BaseOp):
         return np.mean(x, self.axis)
 
     def gradient(self, grad):
-        sum_grad = SumOp.gradient_static(self, grad)
+        sum_grad = SumOp.gradient_static(self, grad)[0]
 
         input_shape = self.inputs[0].shape
         output_shape = self.output.shape
@@ -424,7 +409,7 @@ class GroupOp(BaseOp):
 
     def __init__(self, inputs, graph, axes=None, name=None):
         super(GroupOp, self).__init__(inputs, graph, name)
-        self.output = graph.tensor(shape=None, op=self, name=self.name+'/output')
+        self.output = graph.tensor(shape=(), op=self, name=self.name+'/output')
 
     def compute(self, context):
         return None
@@ -436,7 +421,8 @@ class AssignOp(BaseOp):
 
     def __init__(self, a, b, graph, name=None):
         super(AssignOp, self).__init__([a, b], graph, name)
-        self.output = graph.tensor(shape=None, op=self, name=self.name+'/output')
+        assert np.array_equal(self.inputs[0].shape, self.inputs[1].shape), 'Tensors must have the same shape: {} != {}'.format(self.inputs[0], self.inputs[1])
+        self.output = graph.tensor(shape=(), op=self, name=self.name+'/output')
 
     def compute(self, context):
         # TODO: use context, not value which should just be init

session.py
@@ -11,12 +11,14 @@ class Session(object):
         self.graph = graph
 
     def run_op(self, op, context):
+        # print('run_op', op)
         for input_ in op.inputs:
             if input_ not in context:
                 context[input_] = self.eval_tensor(input_, context)
         return op.compute(context)
 
     def eval_tensor(self, tensor, context):
+        # print('eval_tensor', tensor.op, tensor)
         if tensor not in context:
             if tensor.op is not None:
                 context[tensor] = self.run_op(tensor.op, context)
@@ -25,10 +27,9 @@ class Session(object):
         if tensor not in context:
             raise ValueError('Tensor has no value: {}'.format(tensor))
         result = context[tensor]
-        if isinstance(result, np.ndarray):
-            assert np.array_equal(tensor.shape, result.shape), 'Tensor value did not match Tensor shape: {} != {}'.format(tensor, result.shape)
-        else:
-            assert np.array_equal(tensor.shape, ()), 'Tensor value did not match Tensor shape: {} != {}'.format(tensor, result)
+
+        assert np.array_equal(tensor.shape, result.shape if isinstance(result, np.ndarray) else ()), 'Tensor value did not match Tensor shape: {} != {}'.format(tensor, result)
+
         return result
 
     def run(self, fetches, feed_dict=None):

tests/test_ops.py
@@ -13,8 +13,8 @@ class OpsTestCase(unittest.TestCase):
     def test_add(self):
         graph = Graph()
 
-        a = graph.tensor()
-        b = graph.tensor()
+        a = graph.tensor(shape=())
+        b = graph.tensor(shape=())
         c = a + b
 
         sess = Session(graph)
@@ -28,8 +28,8 @@ class OpsTestCase(unittest.TestCase):
     def test_sub(self):
         graph = Graph()
 
-        a = graph.tensor()
-        b = graph.tensor()
+        a = graph.tensor(shape=())
+        b = graph.tensor(shape=())
         c = a - b
 
         sess = Session(graph)
@@ -43,8 +43,8 @@ class OpsTestCase(unittest.TestCase):
     def test_mul(self):
         graph = Graph()
 
-        a = graph.tensor()
-        b = graph.tensor()
+        a = graph.tensor(shape=())
+        b = graph.tensor(shape=())
         c = a * b
 
         sess = Session(graph)
@@ -58,8 +58,8 @@ class OpsTestCase(unittest.TestCase):
     def test_div(self):
         graph = Graph()
 
-        a = graph.tensor()
-        b = graph.tensor()
+        a = graph.tensor(shape=())
+        b = graph.tensor(shape=())
         c = a / b
 
         sess = Session(graph)
@@ -73,7 +73,7 @@ class OpsTestCase(unittest.TestCase):
     def test_square(self):
         graph = Graph()
 
-        a = graph.tensor()
+        a = graph.tensor(shape=())
         b = graph.square(a)
 
         sess = Session(graph)
@@ -86,7 +86,7 @@ class OpsTestCase(unittest.TestCase):
     def test_power(self):
         graph = Graph()
 
-        a = graph.tensor()
+        a = graph.tensor(shape=())
         b = graph.power(a, 3)
 
         sess = Session(graph)
@@ -99,7 +99,7 @@ class OpsTestCase(unittest.TestCase):
     def test_log(self):
         graph = Graph()
 
-        a = graph.tensor()
+        a = graph.tensor(shape=())
         b = graph.log(a)
 
         sess = Session(graph)
@@ -112,7 +112,7 @@ class OpsTestCase(unittest.TestCase):
     def test_neg(self):
         graph = Graph()
 
-        a = graph.tensor()
+        a = graph.tensor(shape=())
         b = graph.neg(a)
 
         sess = Session(graph)
@@ -125,7 +125,7 @@ class OpsTestCase(unittest.TestCase):
     def test_sigmoid(self):
         graph = Graph()
 
-        a = graph.tensor()
+        a = graph.tensor(shape=())
         b = graph.sigmoid(a)
 
         sess = Session(graph)
@@ -138,7 +138,7 @@ class OpsTestCase(unittest.TestCase):
     def test_softmax(self):
         graph = Graph()
 
-        a = graph.tensor()
+        a = graph.tensor(shape=(3,))
         b = graph.softmax(a)
 
         sess = Session(graph)
@@ -150,7 +150,7 @@ class OpsTestCase(unittest.TestCase):
     def test_relu(self):
         graph = Graph()
 
-        a = graph.tensor()
+        a = graph.tensor(shape=(3,))
         b = graph.relu(a)
 
         sess = Session(graph)
@@ -162,7 +162,7 @@ class OpsTestCase(unittest.TestCase):
     def test_where(self):
         graph = Graph()
 
-        a = graph.tensor()
+        a = graph.tensor(shape=(3,))
         b = graph.where(a > 0, 1, 0)
 
         sess = Session(graph)
@@ -174,8 +174,8 @@ class OpsTestCase(unittest.TestCase):
     def test_equal(self):
         graph = Graph()
 
-        a = graph.tensor()
-        b = graph.tensor()
+        a = graph.tensor(shape=(3,))
+        b = graph.tensor(shape=(3,))
         c = graph.equal(a, b)
 
         sess = Session(graph)
@@ -190,8 +190,8 @@ class OpsTestCase(unittest.TestCase):
     def test_argmax(self):
         graph = Graph()
 
-        a = graph.tensor()
-        c = graph.argmax(a, axis=0)
+        a = graph.tensor(shape=())
+        c = graph.argmax(a)
 
         sess = Session(graph)
 
@@ -202,7 +202,7 @@ class OpsTestCase(unittest.TestCase):
     def test_greater(self):
         graph = Graph()
 
-        a = graph.tensor()
+        a = graph.tensor(shape=(3,))
         b = a > 0
 
         sess = Session(graph)
@@ -214,7 +214,7 @@ class OpsTestCase(unittest.TestCase):
     def test_dot(self):
         graph = Graph()
 
-        a = graph.tensor(value=np.array([0, 1, 2, 3]))
+        a = graph.tensor(value=np.array([0, 1, 2, 3]).reshape((1, -1)))
         b = graph.tensor(value=np.array([0, 1, 2, 3]).reshape((-1, 1)))
         c = graph.dot(a, b)
 
@@ -222,7 +222,7 @@ class OpsTestCase(unittest.TestCase):
 
         c_, = sess.run([c])
 
-        self.assertTrue(np.array_equal(c_, [14]))
+        self.assertTrue(np.array_equal(c_, [[14]]))
 
     def test_transpose(self):
         graph = Graph()